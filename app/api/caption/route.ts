41cbecca9743ae4916a5ba513980807d2174ef4e
25LP1284 25LP1284 <tergelbaasan4@gmail.com>
2026-02-27 09:04:04 +0800

    asdf

---
+++ app/api/caption/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  try {
+    return NextResponse.json({ result: "daraa ni ooroo soli" });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
